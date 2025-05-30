ced62e04beb79de753b2fe900441edb90e34fb39
Ashwin Chandeep.B <ab6264@srmist.edu.in>
2025-05-30 18:09:56 +0530

    Add files via upload

---
+++ Core Java/explicittypecasting.java
@@ -0,0 +1,10 @@
+package javacore;
+
+public class explicittypecasting {
+    public static void main(String[] args) {
+        float a = 5.0f;
+        int b = (int) a;  
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b (after casting): " + b);
+    }
+}
